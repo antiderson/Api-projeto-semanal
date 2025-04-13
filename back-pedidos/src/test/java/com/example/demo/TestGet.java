cd77a2b827f6994543427761025e0e0d2d0b27d0
Rafael Ch <rafael.cherman0@gmail.com>
2025-04-13 16:01:20 -0300

    teste4

--- back-pedidos/src/test/java/com/example/demo/TestGet.java
+++ back-pedidos/src/test/java/com/example/demo/TestGet.java
@@ -3,11 +3,11 @@ package com.example.demo;
 import com.example.demo.controller.PedidoController;
 import com.example.demo.entity.Pedido;
 import com.example.demo.respository.PedidoRepository;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -16,24 +16,23 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class TestGet {
 
     @MockBean
-    private PedidoRepository pedidoRepository; // Mockamos o Repository real
+    private PedidoRepository pedidoRepository;
 
     @Autowired
-    private PedidoController pedidoController; // Injetamos o Controller real
+    private PedidoController pedidoController;
 
     protected static List<Pedido> criarPedidoList(){
-
         Pedido pedido = new Pedido();
         pedido.setCliente("nome");
         pedido.setEmail("nome@email.com");
 
         List<Pedido> pedidos = new ArrayList<>();
         pedidos.add(pedido);
-
         return pedidos;
     }
 
