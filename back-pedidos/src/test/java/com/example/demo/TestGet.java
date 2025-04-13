cdd81be81b4da3e3f9ba3ebc348e28aa3ef6bf20
Rafael Ch <rafael.cherman0@gmail.com>
2025-04-13 15:52:58 -0300

    teste3

--- back-pedidos/src/test/java/com/example/demo/TestGet.java
+++ back-pedidos/src/test/java/com/example/demo/TestGet.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 public class TestGet {
 
     @MockBean
