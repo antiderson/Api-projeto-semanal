871da65eacb8e380d2dd55e53bd9b2f2fe30c033
Rafael Ch <rafael.cherman0@gmail.com>
2025-04-15 21:19:39 -0300

    apresentacao

--- front-pedidos/src/App.tsx
+++ front-pedidos/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>Sistema de Pedidos</h1>
+      <h1>Sistema de Pedidos Apresentação</h1>
       <PedidoForm onSuccess={toggleReload} />
       <PedidoList key={reload ? 'a' : 'b'} />
     </div>
